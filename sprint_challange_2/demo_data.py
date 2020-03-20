bc1eea325e98329c17feea6bf3e16705d74afe99
amindazad <amindazad@icloud.com>
2020-03-20 09:16:33 -0700

    completed part 1 function stretch goal

--- sprint_challange_2/demo_data.py
+++ sprint_challange_2/demo_data.py
@@ -58,9 +58,23 @@ result2 = curs.execute(query_1).fetchall()
 print("rows where both x and y are at least 5:",result2)
 
 # How many unique values of y are there (hint - COUNT() can accept a keyword DISTINCT)?
-query_3 = '''
+query_2 = '''
 SELECT count(DISTINCT y)
 FROM demo
 '''
-result3 = curs.execute(query_3).fetchall()
+result3 = curs.execute(query_2).fetchall()
 print("unique values of y:",result3)
+
+# Create a function to produce all above (stretch goal)
+def run(x, db="demo_data.sqlite3"):
+    conn = sqlite3.connect(db)
+    curs = conn.cursor()
+    curs.execute(x)
+    answer = curs.execute(x).fetchall()
+    curs.close()
+    conn.commit()
+    return print(answer)
+
+run(query)
+run(query_1)
+run(query_2)
